07b1973ed74ae992ce22045829fa999a414fbfee
ArchXUser <31119762+ArchXUser@users.noreply.github.com>
2020-10-24 22:01:37 +0800

    Update index.py

--- index.py
+++ index.py
@@ -5,6 +5,7 @@ from flask import Flask,jsonify,request,render_template
 information = {"result" : 0}
 app = Flask(__name__)
 @app.route("/api/calc",methods=["GET"])
+@app.route("/",methods=["GET"])
 def start():
 	x = int(request.args["x"])
 	y = int(request.args["y"])
